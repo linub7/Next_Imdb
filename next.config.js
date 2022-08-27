954ef53eaaf8c7a0672e3944ba84b198a58c168c
a1a2a3a4a5 <linub7@gmail.com>
2022-08-27 11:44:12 +0430

    Create Project & install tailwind & react-icons packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+  images: {
+    domains: [''],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
